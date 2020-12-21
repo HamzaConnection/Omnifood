74550db07c56f5be89a8e9753da1bb1004bf4ee0
Hamza Laroussi <HamzaConnection@hotmail.com>
2020-12-21 18:14:06 +0100

    Initial commit

---
+++ resources/js/nav.js
@@ -0,0 +1,37 @@
+
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.querySelector("nav").style.top = "0";
+        } else {
+            document.querySelector("nav").style.top = "-125px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+
+    
+    var menuToogle = function () {
+        
+        var mainNav = document.querySelector(".main-nav");
+        var closedNavIcon = document.querySelector(".closeNavIcon")
+        var openNavIcon = document.querySelector(".openNavIcon")
+        
+        if(mainNav.style.display == "") {
+            mainNav.style.display = "block";
+            openNavIcon.style.display = "none";
+            closedNavIcon.style.display = "block"
+            
+        }
+        else {
+            mainNav.style.display = "";
+            openNavIcon.style.display = "block";
+            closedNavIcon.style.display = "none"
+            
+        }
+         
+    }
+
+
+
+
