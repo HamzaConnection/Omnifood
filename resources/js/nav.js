18dde6e013c2adff3f5b759e9a39f1bb02026bca
Hamza Laroussi <HamzaConnection@hotmail.com>
2020-12-22 17:36:00 +0100

    small change

--- resources/js/nav.js
+++ resources/js/nav.js
@@ -2,6 +2,7 @@
     var prevScrollpos = window.pageYOffset;
     window.onscroll = function () {
         var currentScrollPos = window.pageYOffset;
+        console.log("Current scroll position:" + currentScrollPos)
         if (prevScrollpos > currentScrollPos) {
             document.querySelector("nav").style.top = "0";
         } else {
