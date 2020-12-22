586f86c05ac9814601e5e9b2e0a2f5e98ecfbfab
Hamza Laroussi <HamzaConnection@hotmail.com>
2020-12-22 17:45:10 +0100

    changed if statement

--- resources/js/nav.js
+++ resources/js/nav.js
@@ -2,7 +2,7 @@
     var prevScrollpos = window.pageYOffset;
     window.onscroll = function () {
         var currentScrollPos = window.pageYOffset;
-        if (prevScrollpos > currentScrollPos || currentScrollPos == 0) {
+        if (prevScrollpos > currentScrollPos || currentScrollPos < 5) {
             document.querySelector("nav").style.top = "0";
         } else {
             document.querySelector("nav").style.top = "-125px";
